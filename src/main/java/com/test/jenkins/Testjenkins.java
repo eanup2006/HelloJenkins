3d84bb19e9cda93e8454365caad005ec713d3392
anup.sarkar <anup.sarkar@BLRKEC200817L.ad.infosys.com>
2018-05-11 17:27:16 +0530

    ok

--- src/main/java/com/test/jenkins/Testjenkins.java
+++ src/main/java/com/test/jenkins/Testjenkins.java
@@ -4,6 +4,6 @@ public class Testjenkins {
 	
 	public static void main(String args[])
 	{
-		System.out.println("hello Anup jenkins in Github");
+		System.out.println("hello Anup jenkins in Github...");
 	}
 }
